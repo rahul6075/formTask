d07ab2c69eb81b3d63e31e9396c5265aa135a2e9
codestoreRahul <rahul.ch@codestoresolutions.com>
2024-04-02 19:27:15 +0530

    Form Added

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,40 @@ import {
 } from "@/components/ui/card";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./components/ui/select";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import { Button } from "./components/ui/button";
 import "./style.css";
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isSubmitting },
+  } = useForm<ValidationSchemaType>({
+    resolver: zodResolver(validationSchema),
+    defaultValues: initialFormValues,
+  });
+
+  const onSubmit: SubmitHandler<ValidationSchemaType> = (data) => {
+    console.log(data); // You can replace this with your form submission logic
+    localStorage.setItem("formData", JSON.stringify(data)); // Save form data to local storage
+  };
+
+  const handleReset = () => {
+    reset(initialFormValues);
+    // localStorage.removeItem("formData");
+  };
+
+  const handleFetch = () => {
+    const formData = localStorage.getItem("formData") ?? "";
+    reset(JSON.parse(formData));
+  };
+
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen px-10">
       <Card className="w-[550px] border border-gray-300 shadow-lg flex flex-col justify-between">
         <div>
           <CardHeader>
@@ -31,43 +53,68 @@ function App() {
               <div className="grid w-full items-center gap-4">
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Name" />
+                  <Input id="name" placeholder="Name" {...register("name")} />
+                  {errors.name && (
+                    <span className="text-red-500">{errors.name.message}</span>
+                  )}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" placeholder="Your Email" />
+                  <Input
+                    id="email"
+                    placeholder="Your Email"
+                    {...register("email")}
+                  />
+                  {errors.email && (
+                    <span className="text-red-500">{errors.email.message}</span>
+                  )}
                 </div>
                 <div className="flex flex-col space-y-1.5">
                   <Label htmlFor="experience">Experience</Label>
                   <Input
                     id="experience"
-                    type="number"
-                    placeholder="Experience(in years)"
+                    type="text"
+                    placeholder="Experience (in years)"
+                    {...register("experience")}
                   />
+                  {errors.experience && (
+                    <span className="text-red-500">
+                      {errors.experience.message}
+                    </span>
+                  )}
                 </div>
                 <div className="flex flex-col space-y-1.5">
-                  <Label htmlFor="framework">Job Profile</Label>
-                  <Select>
-                    <SelectTrigger id="framework">
-                      <SelectValue placeholder="Select" />
-                    </SelectTrigger>
-                    <SelectContent position="popper">
-                      <SelectItem value="next">NodeJs Developer</SelectItem>
-                      <SelectItem value="sveltekit">
-                        Full Stack Developer
-                      </SelectItem>
-                      <SelectItem value="astro">Python Developer</SelectItem>
-                      <SelectItem value="nuxt">React JS Developer</SelectItem>
-                    </SelectContent>
-                  </Select>
+                  <Label htmlFor="resume">Resume</Label>
+                  <Input
+                    id="resume"
+                    type="file"
+                    accept=".pdf,.doc,.docx"
+                    {...register("resume")}
+                  />
+                  {errors.resume && (
+                    <span className="text-red-500">
+                      {"Image file validation error!"}
+                    </span>
+                  )}
                 </div>
               </div>
             </form>
           </CardContent>
         </div>
         <CardFooter className="flex justify-end gap-5">
-          <Button variant="outline">Reset</Button>
-          <Button>Submit</Button>
+          <Button type="button" variant="outline" onClick={handleReset}>
+            Reset
+          </Button>
+          <Button
+            type="button"
+            onClick={handleSubmit(onSubmit)}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit"}
+          </Button>
+          <Button type="button" onClick={handleFetch}>
+            Fetch
+          </Button>
         </CardFooter>
       </Card>
     </div>
@@ -75,3 +122,28 @@ function App() {
 }
 
 export default App;
+
+const validationSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.string().email("Invalid email address"),
+  experience: z.string().min(0, { message: "Experience cannot be negative" }),
+  resume: z.custom((file: any) => {
+    if (!file) {
+      return "Image is required";
+    }
+    const acceptedImageTypes = ["image/jpeg", "image/png", "image/gif"];
+    if (!acceptedImageTypes.includes(file.type)) {
+      return "Invalid file type. Please upload an image (JPEG, PNG, or GIF)";
+    }
+    return true;
+  }),
+});
+
+type ValidationSchemaType = z.infer<typeof validationSchema>;
+
+const initialFormValues: ValidationSchemaType = {
+  name: "",
+  email: "",
+  experience: "",
+  resume: "",
+};
